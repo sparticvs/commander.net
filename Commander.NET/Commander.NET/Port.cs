4a06b5fdee1ddd99f43283dbc0dc5066803e5372
sparticvs <sparticvs@popebp.com>
2013-12-11 13:29:20 -0500

    Add Port structure and builder

---
+++ Commander.NET/Commander.NET/Port.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Commander.NET
+{
+    public class Port
+    {
+        public int Id { get; set; }
+        public List<Vlan> Vlans { get; set; }
+        public string Details { get; set; }
+
+        public Port()
+        {
+            this.Vlans = new List<Vlan>();
+        }
+    }
+}
---
+++ Commander.NET/Commander.NET/PortBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Commander.NET
+{
+    public static class PortBuilder
+    {
+        public static Port buildPort(int id)
+        {
+            return PortBuilder.buildPort(id, null, "");
+        }
+
+        public static Port buildPort(int id, List<Vlan> vlans)
+        {
+            return PortBuilder.buildPort(id, vlans, "");
+        }
+
+        public static Port buildPort(int id, List<Vlan> vlans, string detail)
+        {
+            Port port = new Port();
+            port.Details = detail;
+            port.Id = id;
+            port.Vlans.Clear();
+            if (vlans != null)
+            {
+                port.Vlans.AddRange(vlans);
+            }
+            return port;
+        }
+    }
+}
