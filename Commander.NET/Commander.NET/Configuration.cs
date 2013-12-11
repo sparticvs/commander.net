6d3116ec0ac7140501f79dae92794bf13a15b1bb
sparticvs <sparticvs@popebp.com>
2013-12-11 16:48:51 -0500

    Add Configuration Code

---
+++ Commander.NET/Commander.NET/Configuration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+
+namespace Commander.NET
+{
+    public class Configuration
+    {
+        public string Name { get; set; }
+        public string Serial { get; set; }
+        public string Hostname { get; set; }
+        public string OpenPortName { get; set; }
+        public Color OpenPortColor { get; set; }
+        public List<Vlan> Vlans { get; set; }
+        public List<Port> Ports { get; set; }
+
+        public Configuration()
+        {
+            this.Vlans = new List<Vlan>();
+            this.Ports = new List<Port>();
+        }
+    }
+}
---
+++ Commander.NET/Commander.NET/ConfigurationFactory.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+using System.Xml;
+
+namespace Commander.NET
+{
+    public static class ConfigurationFactory
+    {
+        public static Configuration createConfigurationFromFile(string fileName)
+        {
+            Configuration config = new Configuration();
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(fileName);
+
+            XmlNode node = doc.SelectSingleNode("/switch");
+            config.Name = node.Attributes["name"].Value;
+            config.Serial = node.Attributes["serial"].Value;
+            config.Hostname = node.Attributes["hostname"].Value;
+            node = doc.SelectSingleNode("/switch/open_port");
+            config.OpenPortName = node.Attributes["name"].Value;
+            config.OpenPortColor = (Color)ColorConverter.ConvertFromString(node.Attributes["color"].Value);
+
+            XmlNodeList nodes = doc.SelectNodes("/switch/vlans");
+            foreach (XmlNode n in nodes)
+            {
+                config.Vlans.Add(VlanFactory.createVlan(n.Attributes["name"].Value,
+                                                        n.Attributes["id"].Value,
+                                                        n.Attributes["color"].Value));
+            }
+
+            nodes = doc.SelectNodes("/switch/ports");
+            foreach (XmlNode n in nodes)
+            {
+                n.Attributes["id"];
+                n.Attributes["vlans"];
+                n.InnerText;
+            }
+            
+
+
+            return null;
+        }
+    }
+}
