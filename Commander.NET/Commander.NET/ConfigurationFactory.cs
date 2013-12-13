0360cb1cc904b0a7d2b0435a63013adaca2be979
sparticvs <sparticvs@popebp.com>
2013-12-13 14:37:33 -0500

    Add Open/Save Config Steps

--- Commander.NET/Commander.NET/ConfigurationFactory.cs
+++ Commander.NET/Commander.NET/ConfigurationFactory.cs
@@ -35,14 +35,103 @@ namespace Commander.NET
             nodes = doc.SelectNodes("/switch/ports");
             foreach (XmlNode n in nodes)
             {
-                n.Attributes["id"];
-                n.Attributes["vlans"];
-                n.InnerText;
+                Port p = PortBuilder.buildPort(n.Attributes["id"].Value, n.InnerText);
+                string[] vlan_ids = n.Attributes["vlans"].Value.Split(',');
+
+                foreach (string vid in vlan_ids)
+                {
+                    int vlan_id = int.Parse(vid);
+                    p.Vlans.Add(config.Vlans.Find(x => x.Id == vlan_id));
+                }
+
+                config.Ports.Add(p);
             }
-            
 
+            return config;
+        }
 
-            return null;
+        public static void saveConfigurationToFile(string fileName, Configuration config)
+        {
+            XmlDocument doc = new XmlDocument();
+
+            XmlNode rootNode = doc.CreateElement("switch");
+
+            XmlAttribute attr = doc.CreateAttribute("name");
+            attr.Value = config.Name;
+            rootNode.Attributes.Append(attr);
+
+            attr = doc.CreateAttribute("serial");
+            attr.Value = config.Serial;
+            rootNode.Attributes.Append(attr);
+
+            attr = doc.CreateAttribute("hostname");
+            attr.Value = config.Hostname;
+            rootNode.Attributes.Append(attr);
+
+            XmlNode opNode = doc.CreateElement("open_port");
+
+            attr = doc.CreateAttribute("name");
+            attr.Value = config.OpenPortName;
+            opNode.Attributes.Append(attr);
+
+            attr = doc.CreateAttribute("color");
+            attr.Value = config.OpenPortColor.ToString();
+            opNode.Attributes.Append(attr);
+
+            rootNode.AppendChild(opNode);
+
+            XmlNode vlansNode = doc.CreateElement("vlans");
+
+            foreach (Vlan vlan in config.Vlans)
+            {
+                XmlNode vlanNode = doc.CreateElement("vlan");
+
+                attr = doc.CreateAttribute("name");
+                attr.Value = vlan.Name;
+                vlanNode.Attributes.Append(attr);
+
+                attr = doc.CreateAttribute("id");
+                attr.Value = vlan.Id.ToString();
+                vlanNode.Attributes.Append(attr);
+
+                attr = doc.CreateAttribute("color");
+                attr.Value = vlan.Color.ToString();
+                vlanNode.Attributes.Append(attr);
+
+                vlansNode.AppendChild(vlanNode);
+            }
+
+            rootNode.AppendChild(vlansNode);
+
+            XmlNode portsNode = doc.CreateElement("ports");
+            // TODO: Handle rows, cols, and groups for UI
+            foreach (Port port in config.Ports)
+            {
+                XmlNode portNode = doc.CreateElement("port");
+
+                attr = doc.CreateAttribute("id");
+                attr.Value = port.Id.ToString();
+                portNode.Attributes.Append(attr);
+
+                attr = doc.CreateAttribute("vlans");
+                List<string> vlans = new List<string>();
+                foreach (Vlan v in port.Vlans)
+                {
+                    vlans.Add(v.Id.ToString());
+                }
+                attr.Value = string.Join(",", vlans);
+                portNode.Attributes.Append(attr);
+
+                portNode.InnerText = port.Details;
+
+                portsNode.AppendChild(portNode);
+            }
+
+            rootNode.AppendChild(portsNode);
+
+            doc.AppendChild(rootNode);
+
+            doc.Save(fileName);
         }
     }
 }
